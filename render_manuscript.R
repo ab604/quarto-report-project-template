18886e8ae84959443d16dbc59866aa38b78e0063
Alistair Bailey <ab604@soton.ac.uk>
2023-03-13 09:32:17 +0000

    update render script with Pathmeswaran's fix

--- render_manuscript.R
+++ render_manuscript.R
@@ -1,13 +1,7 @@
 # Render quarto 
 library(quarto)
 library(here)
-# Quarto can't do relative paths in Windows yet.
-# Compile and render the manuscript with today's date
-quarto_render(here("index.qmd"), 
-              output_file = paste0("manuscript-draft-",
-                                        Sys.Date(),".docx"))
-
-# Move it to the drafts folder
-fs::file_move(paste0("manuscript-draft-",
-                     Sys.Date(),".docx"),paste0("drafts/manuscript-draft-",
-                                                Sys.Date(),".docx"))
+# Compile and render the manuscript with today's date using Pathmeswaran's fix.
+quarto_render(here("index.qmd"),
+              output_file = here("drafts",
+                                 paste0("manuscript-draft-", Sys.Date(), ".docx")))
